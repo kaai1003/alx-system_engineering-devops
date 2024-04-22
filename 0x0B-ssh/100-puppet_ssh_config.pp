cd98ba2aa6b7d48c2bdc43b3df4cc2da827ea4b7
AIMAD KACEM <kacem.aimad@gmail.com>
2024-04-22 17:57:16 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 file_line {'Turn off passwd auth':
   ensure => 'present',
   line   => '  BatchMode yes',
-  path   => '/etc/ssh/ssh_confi',
+  path   => '/etc/ssh/ssh_config',
 }
 
 file_line {'Declare identity file':
   ensure => 'present',
   line   => '  IdentityFile ~/.ssh/school',
-  path   => '/etc/ssh/ssh_confi',
+  path   => '/etc/ssh/ssh_config',
 }
