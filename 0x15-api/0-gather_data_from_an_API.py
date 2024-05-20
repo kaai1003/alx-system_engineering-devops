646e26fb9551313349dfa41e9716cd3110dd2126
AIMAD KACEM <kacem.aimad@gmail.com>
2024-05-21 00:58:10 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """display TODOS progress for given employee id"""
 import json
+import requests
 import sys
-import urllib.request
 
 
 if __name__ == "__main__":
@@ -15,20 +15,20 @@ if __name__ == "__main__":
             exit(-1)
         # request user informations
         url_u = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-        r = urllib.request.urlopen(url_u)
-        user = json.loads(r.read())
+        r_user = requests.get(url_u)
+        user = r_user.json()
         user_name = user.get("name")
         # request TODO tasks of user
         url_t = "https://jsonplaceholder.typicode.com/todos"
-        r = urllib.request.urlopen(url_t)
-        todos = json.loads(r.read())
+        r_todos = requests.get(url_t)
+        todos = r_todos.json()
         list_tasks = []
         task_count = 0
         completed_tasks = 0
         for todo in todos:
             if todo.get("userId") == id:
                 task_count += 1
-                if todo.get("completed") is True:
+                if todo.get("completed"):
                     list_tasks.append(todo.get("title"))
                     completed_tasks += 1
         print("Employee {} is done with tasks({}/{}):".format(user_name,
